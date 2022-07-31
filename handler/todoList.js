c80de2fb4b83e522732ed1fc625058d3c42ba6e8
tokatu4561 <ryobsk4561@gmail.com>
2022-07-30 20:31:16 -0700

    get todo list

---
+++ handler/todoList.js
@@ -0,0 +1,26 @@
+const AWS = require("aws-sdk");
+
+const TODO_TABLE = process.env.TODO_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+exports.createTodo = (event, context, callback) =>  { 
+    const params = {
+        TableName: TODO_TABLE,
+    }
+
+    dynamoDb.scan(params, (error, data) => {
+        if (error) {
+            console.error(error);
+            callback(new Error);
+            return
+        }
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data.Items)
+        }
+
+        callback(null, response)
+    })
+
+}
